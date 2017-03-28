b601cd959828fa5fc3580fd9d6b97bac829cbc20
rakibulislam <rakib1987@gmail.com>
2017-03-28 17:57:07 -0400

    Add support for bill delete and bulk refresh

---
+++ test/tapes/bill/delete.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = function (req, res) {
+  res.statusCode = 200;
+
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('X-Xss-Protection', '1; mode=block');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.setHeader('Etag', '"7d6d90aec27891b906d0ee6dfc9af4f3"');
+  res.setHeader('Cache-Control', 'max-age=0, private, must-revalidate');
+  res.setHeader('X-Request-Id', '7fbe6044-0ea6-4384-abc6-5c0db31566b1');
+  res.setHeader('X-Runtime', '0.026813');
+  res.setHeader('Vary', 'Origin');
+  res.setHeader('Date', 'Wed, 23 Mar 2016 00:31:47 GMT');
+  res.setHeader('Connection', 'close');
+
+  res.write(new Buffer(JSON.stringify({
+    "id": 5,
+    "biller_id": 2,
+    "account_number": "8081969",
+    "name_on_account": "Test name",
+    "due_date": null,
+    "balance": 44.0,
+    "balance_currency": "RD",
+    "balance_in_chain_currency": 1.01,
+    "balance_updated_at": "2016-03-22T21:38:08Z",
+    "created_at": "2016-03-22T21:32:36Z",
+    "last_paid_at": null,
+    "error_with_account": false,
+    "error_message": null,
+    "status": "updated",
+    "mfa_challenges": []
+  })));
+  res.end();
+
+  return __filename;
+};
